1ba3b8672aa22b9192a3fe101e48b3dcc644185b
dexterousprogrammers <tobiburrohman2@gmail.com>
2022-06-13 10:02:57 +0600

    first

---
+++ src/Pages/Shared/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+  return (
+    <h1 className="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  
+  )
+}
+
+export default Header
