4f568ed60a9b6e03ea781259d40edff19c13c0fc
dexterousprogrammers <tobiburrohman2@gmail.com>
2022-06-16 23:38:56 +0600

    blog page is done

---
+++ src/Pages/Hooks/useBlogs.js
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from 'react'
+
+const useBlogs = () => {
+    const [blogs, setBlogs] = useState([]);
+
+    useEffect(() => {
+        fetch("blogs.json")
+            .then(res => res.json())
+            .then(data => {
+                // console.log(data)
+                setBlogs(data)
+            })
+    }, [])
+
+
+    return [blogs, setBlogs];
+}
+
+export default useBlogs
