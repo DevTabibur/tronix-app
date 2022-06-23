7778422dc3ffcd3e2721f17fd116f9725ba9f205
dexterousprogrammers <tobiburrohman2@gmail.com>
2022-06-23 13:17:18 +0600

    update profile name is done

--- src/Pages/Shared/Register/Register.jsx
+++ src/Pages/Shared/Register/Register.jsx
@@ -1,96 +1,121 @@
 import React from 'react'
-import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { useCreateUserWithEmailAndPassword, useSignInWithEmailAndPassword, useSignInWithGoogle, useUpdateProfile } from 'react-firebase-hooks/auth';
 import { useForm } from 'react-hook-form';
+import { useNavigate } from 'react-router-dom';
 import auth from '../../../Firebase/firebase.init';
+import Loading from '../Loading/Loading';
 
 const Register = () => {
+  const navigate = useNavigate();
   const [
     createUserWithEmailAndPassword,
     cUser,
     cLoading,
     cError,
   ] = useCreateUserWithEmailAndPassword(auth);
+  const [signInWithGoogle, gUser, gLoading, gError] = useSignInWithGoogle(auth);
+  const [
+    signInWithEmailAndPassword,
+    eUser,
+    eLoading,
+    eError,
+  ] = useSignInWithEmailAndPassword(auth);
+  const [updateProfile, updating, updateError] = useUpdateProfile(auth);
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
 
-    const {
-        register,
-        formState: { errors },
-        handleSubmit,
-      } = useForm();
-git
-      const onSubmit = (data) => {
-        console.log(data);
-        createUserWithEmailAndPassword(data.email, data.password)
-      };
+  const onSubmit = async (data) => {
+    await createUserWithEmailAndPassword(data.email, data.password);
+    await updateProfile({ displayName: data.name });
+    navigate("/home");
+    console.log(data);
+
+
+  };
+
+  // for loader
+  if (gLoading || eLoading || updating) {
+    return <Loading />
+  }
+
+  // for error showing messages
+  let signInError;
+  if (eError || gError || updateError) {
+    signInError = <small><p className="text-red-500">{eError?.message || gError?.message || updateError?.message}</p></small>
+  }
 
   return (
     <div className="register-form">
-              <div className="left-col border rounded-lg">
-                <div className="login-form">
-                  <h1 className="text-2xl form-title ml-14 my-3">Register form</h1>
-                  <p className="form-sub-title mb-3 ml-14">
-                    Are you new here? Please Register!
-                  </p>
+      <div className="left-col border rounded-lg">
+        <div className="login-form">
+          <h1 className="text-2xl form-title ml-14 my-3">Register form</h1>
+          <p className="form-sub-title mb-3 ml-14">
+            Are you new here? Please Register!
+          </p>
 
-                  <form onSubmit={handleSubmit(onSubmit)}>
+          <form onSubmit={handleSubmit(onSubmit)}>
 
-                    <label htmlFor="email" className=" my-0 py-0">Name</label>
-                    <input
-                      type="text"
-                      placeholder="Your Name"
-                      // className="input input-bordered w-full max-w-xs"
-                      {...register("name", {
-                        required: {
-                          value: true,
-                          message: 'Name is Required'
-                        }
-                      })}
-                    />
-                    <label className="label my-0 py-0">
-                      {errors.name?.type === 'required' && <span className="label-text-alt text-red-500">{errors.name.message}</span>}
-                    </label>
+            <label htmlFor="name" className=" my-0 py-0">Name</label>
+            <input
+              type="text"
+              placeholder="Your Name"
+              // className="input input-bordered w-full max-w-xs"
+              {...register("name", {
+                required: {
+                  value: true,
+                  message: 'Name is Required'
+                }
+              })}
+            />
+            <label className="label my-0 py-0">
+              {errors.name?.type === 'required' && <span className="label-text-alt text-red-500">{errors.name.message}</span>}
+            </label>
 
 
-                    <label htmlFor="email" className=" my-0 py-0">Email</label>
-                    <input
-                      type="email"
-                      placeholder="Your Email"
-                      // className="input input-bordered w-full max-w-xs"
-                      {...register("email", {
-                        required: {
-                          value: true,
-                          message: 'Email is Required'
-                        },
-                        pattern: {
-                          value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
-                          message: 'Provide a valid Email'
-                        }
-                      })}
-                    />
-                    <label className="label my-0 py-0">
-                      {errors.email?.type === 'required' && <span className="label-text-alt text-red-500">{errors.email.message}</span>}
-                      {errors.email?.type === 'pattern' && <span className="label-text-alt text-red-500">{errors.email.message}</span>}
-                    </label>
+            <label htmlFor="email" className=" my-0 py-0">Email</label>
+            <input
+              type="email"
+              placeholder="Your Email"
+              // className="input input-bordered w-full max-w-xs"
+              {...register("email", {
+                required: {
+                  value: true,
+                  message: 'Email is Required'
+                },
+                pattern: {
+                  value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
+                  message: 'Provide a valid Email'
+                }
+              })}
+            />
+            <label className="label my-0 py-0">
+              {errors.email?.type === 'required' && <span className="label-text-alt text-red-500">{errors.email.message}</span>}
+              {errors.email?.type === 'pattern' && <span className="label-text-alt text-red-500">{errors.email.message}</span>}
+            </label>
 
-                    <label htmlFor="password" className=" my-0 py-0">Password</label>
-                    <input
-                      type="password"
-                      placeholder="Enter Password"
-                      {...register("password", {
-                        required: {
-                          value: true,
-                          message: 'Password is Required'
-                        },
-                        minLength: {
-                          value: 6,
-                          message: 'Must be 6 characters or longer'
-                        }
-                      })} />
-                    <label className="label my-0 py-0">
-                      {errors.password?.type === 'required' && <span className="label-text-alt text-red-500">{errors.password.message}</span>}
-                      {errors.password?.type === 'minLength' && <span className="label-text-alt text-red-500">{errors.password.message}</span>}
-                    </label>
+            <label htmlFor="password" className=" my-0 py-0">Password</label>
+            <input
+              type="password"
+              placeholder="Enter Password"
+              {...register("password", {
+                required: {
+                  value: true,
+                  message: 'Password is Required'
+                },
+                minLength: {
+                  value: 6,
+                  message: 'Must be 6 characters or longer'
+                }
+              })} />
+            <label className="label my-0 py-0">
+              {errors.password?.type === 'required' && <span className="label-text-alt text-red-500">{errors.password.message}</span>}
+              {errors.password?.type === 'minLength' && <span className="label-text-alt text-red-500">{errors.password.message}</span>}
+            </label>
 
-                    {/* <label htmlFor="confirm password" className=" my-0 py-0">Confirm Password</label>
+            {/* <label htmlFor="confirm password" className=" my-0 py-0">Confirm Password</label>
                     <input
                       type="password"
                       placeholder="Confirm Password"
@@ -108,14 +133,14 @@ git
                       {errors.password?.type === 'required' && <span className="label-text-alt text-red-500">{errors.password.message}</span>}
                       {errors.password?.type === 'minLength' && <span className="label-text-alt text-red-500">{errors.password.message}</span>}
                     </label> */}
+            {signInError}
+            <input type="submit" value="REGISTER" />
 
-                    <input type="submit" value="REGISTER" />
+          </form>
+        </div>
+      </div>
 
-                  </form>
-                </div>
-              </div>
-
-            </div>
+    </div>
   )
 }
 
