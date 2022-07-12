3200a341a1095c3a8c24ed9d443c5505e38f476d
unknown <tobiburrohman2@gmail.com>
2022-07-13 01:24:11 +0600

    data loaded

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Category from '../Shared/Category/Category';
-import HomeProducts from '../Shared/HomeProducts/HomeProducts';
 import Shop from '../Shop/Shop';
 import Banner from './Banner';
 import './Home.css';
@@ -9,9 +8,9 @@ const Home = () => {
   return (
     <>
         {/* <Banner/> */}
-        {/* <HomeProducts/> */}
-        {/* <Shop/> */}
         <Category/>
+        <Shop/>
+
     </>
   )
 }
--- src/Pages/Shop/Shop.jsx
+++ src/Pages/Shop/Shop.jsx
@@ -1,5 +1,9 @@
-import React, { useEffect, useState } from 'react';
-import './Shop.css';
+
+import React, { useEffect, useState } from "react";
+import { useQuery } from "react-query";
+import Loading from "../Shared/Loading/Loading";
+import SinglePD from "../SinglePD/SinglePD";
+import "./Shop.css";
 
 const Shop = () => {
   // showing how many products we want to show in a single page
@@ -10,35 +14,54 @@ const Shop = () => {
   // currently what page we are staying?
   const [pages, setPages] = useState(0);
 
-  useEffect(()=>{
-    fetch('http://localhost:5000/productCount')
-    .then(res => res.json())
-    .then(data =>{
-      console.log(data);
-      const count = data.count;
-      const pages = Math.ceil(count/10)
-      setPages(pages)
-    })
-  }, [])
+  useEffect(() => {
+    fetch("http://localhost:5000/productCount")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        const count = data.count;
+        const pages = Math.ceil(count / 10);
+        setPages(pages);
+      });
+  }, []);
+
+  // data load all products
+  const {
+    data: products,
+    isLoading,
+    refetch,
+  } = useQuery("products", () =>
+    fetch("http://localhost:5000/products").then((res) => res.json())
+  );
+  if (isLoading) {
+    return <Loading></Loading>;
+  }
 
   return (
     <>
-        <div className='container mx-auto px-4'>
-        <h2 className='text-3xl'>Products</h2>
-          <div className='pagination-btn text-gray-900 font-bold flex'>
+      <div className="container mx-auto px-4">
+        <h2 className="text-3xl">Products </h2>
+        {/* <div className='pagination-btn text-gray-900 font-bold flex'>
             {
               [...Array(pageCount).keys()].map(number => <button onClick={()=>setPages(number)} className={pages === number ? 'selected': ''}>{number + 1}</button>)
             }
-            {/* {size} */}
+            {size}
             <select className='ml-5' onChange={ e => setSize(e.target.value)}>
               <option value='5'>5</option>
               <option value='10'>10</option>
               <option value='15'>15</option>
             </select>
-          </div>
-        </div>
-    </>
-  )
-}
+          </div> */}
 
-export default Shop
+        
+        <div className="md:grid grid-cols-4 gap-4 py-10">
+        {products.map((pd) => (
+            <SinglePD key={pd._id} pd={pd} refetch={refetch} />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Shop;
